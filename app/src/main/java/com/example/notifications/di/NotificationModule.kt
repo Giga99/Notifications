643161475ecbd6466cee81c6504341ab1a7755af
Giga99 <igorstevanovic99@gmail.com>
2022-09-15 14:37:43 +0200

    Added action notification

--- app/src/main/java/com/example/notifications/di/NotificationModule.kt
+++ app/src/main/java/com/example/notifications/di/NotificationModule.kt
@@ -2,11 +2,14 @@ package com.example.notifications.di
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.VISIBILITY_PRIVATE
 import androidx.core.app.NotificationManagerCompat
 import com.example.notifications.R
+import com.example.notifications.receiver.MyReceiver
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,18 +25,31 @@ object NotificationModule {
     @Provides
     fun provideNotificationBuilder(
         @ApplicationContext context: Context
-    ): NotificationCompat.Builder = NotificationCompat.Builder(context, "Main Channel ID")
-        .setContentTitle("Welcome")
-        .setContentText("Notifications app")
-        .setSmallIcon(R.drawable.ic_notifications)
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .setVisibility(VISIBILITY_PRIVATE)
-        .setPublicVersion(
-            NotificationCompat.Builder(context, "Main Channel ID")
-                .setContentTitle("Hidden")
-                .setContentText("Unlock to see the message.")
-                .build()
+    ): NotificationCompat.Builder {
+        val intent = Intent(context, MyReceiver::class.java).apply {
+            putExtra("MESSAGE", "Clicked!")
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
         )
+
+        return NotificationCompat.Builder(context, "Main Channel ID")
+            .setContentTitle("Welcome")
+            .setContentText("Notifications app")
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setVisibility(VISIBILITY_PRIVATE)
+            .setPublicVersion(
+                NotificationCompat.Builder(context, "Main Channel ID")
+                    .setContentTitle("Hidden")
+                    .setContentText("Unlock to see the message.")
+                    .build()
+            )
+            .addAction(0, "ACTION", pendingIntent)
+    }
 
     @Singleton
     @Provides
